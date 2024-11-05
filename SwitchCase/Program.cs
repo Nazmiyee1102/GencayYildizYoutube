3540546470fcecbdc7baaf63f493ab34de4d18f1
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-11-06 00:59:39 +0300

    #

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,152 @@
+﻿#region switch-case
+
+//string adi = "Ahmet";
+
+//switch (adi)
+//{
+//	case "Mehmet":
+//        Console.WriteLine("Adı Mehmet");
+//		break;
+
+//	case "Ayşe":
+//        Console.WriteLine("Adı Ayşe");
+//		break;
+
+//    case "Hilmi":
+//        Console.WriteLine("Adı Hilmi");
+//        break;
+
+//    default:
+//        Console.WriteLine(""); 
+//        break;
+//}
+
+#endregion
+
+#region when
+
+//int satisTutari = 1000;
+//switch (satisTutari)
+//{
+//	case 1000 when (3 == 5):
+//		break;
+
+//	case 1000 when (3 == 3):
+//		break;
+//}
+
+#endregion
+
+#region goto
+
+//int i = 10;
+//switch (i)
+//{
+//    case 5:
+//        Console.WriteLine(i * 10);
+//        break;
+//    case 6:
+//        Console.WriteLine(i / 5);
+//        break;
+//    case 7:
+//     //   goto case 5; //Console.WriteLine(i * 10); birleştirmek için bu satıra gerek yok
+//       // break;
+//    case 10:
+//        goto case 5;  //Console.WriteLine(i * 10);
+//       // break;
+//}
+
+#endregion
+
+#region Switch Expression
+
+string mesaj = "";
+//eski yöntem
+switch (DateTime.Now.DayOfWeek)
+{
+	case DayOfWeek.Sunday:
+		mesaj = "Bugün Pazar";
+		break;
+	case DayOfWeek.Monday:
+        mesaj = "Bugün Pazartesi";
+        break;
+	case DayOfWeek.Tuesday:
+        mesaj = "Bugün Salı";
+        break;
+	case DayOfWeek.Wednesday:
+        mesaj = "Bugün Çarşamba";
+        break;
+	case DayOfWeek.Thursday:
+        mesaj = "Bugün Perşembe";
+        break;
+	case DayOfWeek.Friday:
+        mesaj = "Bugün Cuma";
+        break;
+	case DayOfWeek.Saturday:
+        mesaj = "Bugün Cumartesi";
+        break;
+	default:
+		break;
+}
+
+//switch expression yeni yöntem
+string mesaj2 = DateTime.Now.DayOfWeek switch
+{
+    DayOfWeek.Monday => "Bugün Pazartesi",
+    DayOfWeek.Tuesday => "Bugün Salı",
+    DayOfWeek.Wednesday => "Bugün Çarşamba",
+    DayOfWeek.Thursday => "Bugün Perşembe",
+    DayOfWeek.Friday => "Bugün Cuma",
+    DayOfWeek.Saturday => "Bugün Cumartesi",
+    DayOfWeek.Sunday => "Bugün Pazar"
+};
+Console.WriteLine(mesaj);
+
+#endregion
+
+#region switch expression ve when anahtar kelimesi
+
+int i = 10;
+string isim = i switch
+{
+    5 when 3 == 3 => "Hilmi",
+    var x when x == 7 & x % 2 == 1 => "Rıfkı",
+    10 => "Gençay",
+    var x => "Hiçbiri"//dafult un karşılığı
+};
+
+#endregion
+
+#region tuple patterns
+
+int sayi1 = 10, sayi2 = 20;
+//string mesaj3 = "";
+//switch (sayi1, sayi2)
+//{
+//    case (5,10):
+//        mesaj = "5 ile 10 değerleri ";
+//        break;
+//    case (10, 20):
+//        mesaj = "10 ile 20 değerleri";
+//        break;
+//}
+
+string message = (sayi1, sayi2) switch
+{
+    (5,10) => "5 ile 10 değerleri",
+    (10,20) => "10 ile 20 değerleri"
+};
+Console.WriteLine(message);
+
+#endregion
+
+#region when şartı ile tuple patterns
+
+string message2 = (sayi1, sayi2) switch
+{
+    (5, 10) when true => "5 ile 10 değerleri",
+    var x when x.sayi1 %2 == 1  | x.sayi2 == 10 => "10 ile 20 değerleri"
+};
+Console.WriteLine(message2);
+
+#endregion
---
+++ if/Program.cs
@@ -0,0 +1,295 @@
+﻿
+
+#region if  
+
+//bool medeniHal = true;
+//if (medeniHal == true)
+//{
+//    Console.WriteLine("Mutlu yıllar");
+//}
+
+#endregion
+
+#region Kritik 1
+
+//int i = 10;
+//if (i != 10)
+//{
+//    Console.WriteLine("Merhaba");
+//}
+//Console.WriteLine("Dünya");
+
+#endregion
+
+#region Kritik 2
+
+//bool medeniHal2 = true;
+//if (medeniHal)
+//{
+//    Console.WriteLine("Hayırlı Olsun");
+//}
+
+#endregion
+
+#region if-else
+
+//int i = 10;
+//if (i > 5)
+//{
+//    Console.WriteLine("i değeri 5'ten büyüktür.");
+//}
+//else
+//{
+//    Console.WriteLine("i değeri 5'ten büyük değildir.");
+//}
+
+#endregion
+
+#region if-else kritik1
+
+
+//int a = 10;
+//if (a != 5)
+//{
+//    Console.WriteLine("a 10'a eşittir.");
+//}
+
+//else
+//{
+//    Console.WriteLine("a 10' a eşit değildir.");
+//}
+#endregion
+
+#region if-else kritik2
+
+//int b = -5;
+//if (b == 5)
+//{
+//    Console.WriteLine("sayı 5");
+//}
+
+//else
+//{
+//    Console.WriteLine("sayı 5 değil");
+//}
+
+//Console.WriteLine("merhaba");
+
+#endregion
+
+#region if-else if
+
+//int sayi = 31;
+//if (sayi > 5 & sayi <= 10)
+//{
+//    Console.WriteLine(sayi * 5);
+//}
+
+//else if (sayi > 10 & sayi <= 20)
+//{
+//    Console.WriteLine(sayi * 10);
+
+//}
+
+//else if (sayi > 20 & sayi <= 30)
+//{
+//    Console.WriteLine(sayi * 20);
+//}
+
+//else
+//{
+//    Console.WriteLine(sayi);
+//}
+
+#endregion
+
+#region if-else if kritik 1
+
+//int sayi = int.Parse(Console.ReadLine());
+//if (sayi == 100 & sayi <= 200)
+//{
+//    Console.WriteLine("100-200 arasında");
+//}
+
+//else if (sayi > 200 & sayi <= 300)
+//{
+//    Console.WriteLine("200-300 arasında");
+//}
+
+//else if (sayi > 300 & sayi <= 400)
+//{
+//    Console.WriteLine("300-400 arasında");
+//}
+
+#endregion
+
+#region scope suz if
+
+//if (true)
+//    Console.WriteLine("jnfkmvh");
+
+#endregion
+
+#region Örnek 1
+
+//klavyeden 2 ürünün fiyatı girildiğinde toplam fiyat 200 tl den 
+//fazla ise, ikinci üründen %25 indirim yaparak ödenecek tutarı 
+//gösteren uygulama
+
+/*
+ int fiyat1, fiyat2;
+Console.Write("1. Ürünün Fiyatı : ");
+fiyat1 = int.Parse(Console.ReadLine());
+
+Console.Write("2. Ürünün Fiyatı : ");
+fiyat2 = int.Parse(Console.ReadLine());
+
+int toplam = fiyat1 + fiyat2;
+
+if (toplam > 200)
+{
+    int odenecekTutar = fiyat1 + (fiyat2 * 75/100);
+    Console.WriteLine(odenecekTutar);
+}
+
+else
+{
+    Console.WriteLine(toplam);
+}
+*/
+#endregion
+
+#region Örnek 2
+
+//belirlenen kullanıcı adı ve şifre doğru girildiğinde "Giriş Başarılı", yanlış giril-
+//diğinde "Girdiğiniz kullanıcı adı veya şifre hatalı" mesajı veren program
+
+//string kullaniciAdi, sifre;
+
+//Console.Write("Kullanıcı adını giriniz : ");
+//kullaniciAdi = Console.ReadLine();
+
+//Console.Write("Şifre giriniz : ");
+//sifre = Console.ReadLine();
+
+////if (kullaniciAdi == "Nazmiye123" & sifre == "123")
+////{
+////    Console.WriteLine("Giriş Başarılı");
+////}
+
+////else
+////{
+////    Console.WriteLine("Girdiğiniz kullanıcı adı veya şifre hatalı");
+////}
+
+////ternary
+//Console.WriteLine(kullaniciAdi == "Nazmiye123" & sifre == "123" ? "Giriş Başarılı" : "Girdiğiniz kullanıcı adı veya şifre hatalı");
+
+#endregion
+
+#region Örnek 3
+
+//kullanıcıdan alınan iki sayının ve yapılacak işlem türünün 
+//(toplama,çıkarma,bölme,çarpma) sonucu hesaplayan program
+
+//Console.Write("1.sayı : ");
+//int sayi1 = int.Parse(Console.ReadLine());
+
+//Console.Write("2.sayı : ");
+//int sayi2 = int.Parse(Console.ReadLine());
+
+//Console.Write("Yapmak istediğiniz işlem : ");
+//char yapilacakIslem = char.Parse(Console.ReadLine());
+
+//if
+//if (yapilacakIslem == '+')
+//{
+//    Console.WriteLine(sayi1 + sayi2);
+//}
+
+//else if (yapilacakIslem == '-')
+//{
+//    Console.WriteLine(sayi1 - sayi2);
+//}
+
+//else if (yapilacakIslem == '*')
+//{
+//    Console.WriteLine(sayi1 * sayi2);
+//}
+
+//else if (yapilacakIslem == '/')
+//{
+//    Console.WriteLine(sayi1 / sayi2);
+//}
+
+//else
+//{
+//    Console.WriteLine("Böyle bir işlem bulunmamaktadır.");
+//}
+
+//switch
+//switch (yapilacakIslem)
+//{
+//    case '+':
+//        Console.WriteLine(sayi1 + sayi2);
+//        break;
+//    case '-':
+//        Console.WriteLine(sayi1 - sayi2);
+//        break;
+//    case '*':
+//        Console.WriteLine(sayi1 * sayi2);
+//        break;
+//    case '/':
+//        Console.WriteLine(sayi1 / sayi2);
+//        break;
+
+//    default:
+//        Console.WriteLine("Böyle bir işlem bulunmamaktadır.");
+//        break;
+//}
+
+//int sonuc = yapilacakIslem switch
+//{
+//    '+' => sayi1 + sayi2,
+//    '-' => sayi1 - sayi2,
+//    '*' => sayi1 * sayi2,
+//    '/' => sayi1 / sayi2,
+//    _ => 'B'//_ discard operatörüdür. hiçbiri anlamına gelir. default un karşılığıdır.
+
+//};
+
+#endregion
+
+#region Örnek 4
+
+//girilen sayının değeri 10 değilse "sayı yanlış" yazan program
+
+//Console.Write("Sayıyı giriniz : ");
+//int sayi = int.Parse(Console.ReadLine());
+
+//if (sayi != 10)
+//{
+//    Console.WriteLine("sayı yanlış");
+//}
+
+
+#endregion
+
+#region Örnek 5
+
+//girilen sayının negatif ya da pozitif olduğunu bulan program
+
+int sayi = int.Parse(Console.ReadLine());
+
+string sonuc = "";
+
+if (sayi < 0)
+    //Console.WriteLine("negatif");
+    sonuc = "negatif";
+else
+    //Console.WriteLine("pozitif");
+    sonuc = "pozitif";
+
+Console.WriteLine(sonuc);
+
+#endregion
