4a61597760850b5311be457fcf95f47fb7ad2748
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-11-13 02:06:07 +0300

    #

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,5 +1,37 @@
 ﻿#region string
 
+//int? a = null;
+//string b = null;//bunun alan tahsisinde yeri yok.
 
+//string name = "";//null ==> bunun var
+
+//string a2 = string.Empty;
+
+
+#endregion
+
+#region isnullorempty
+
+//string x = string.Empty;
+
+//if (x != "" & x is not null)
+//{
+
+//}
+
+//if(string.IsNullOrEmpty(x))
+//{
+
+//}
+
+#endregion
+
+#region isnullorwhitespace
+
+string x = "    ";
+if (string.IsNullOrWhiteSpace(x))
+{
+    
+}
 
 #endregion
